9e0498a5b2d27a7bd5c75d7b481379e0bc40df4c
Callie Giles <anchoredinthecrown@gmail.com>
2023-09-20 11:24:35 -0400

    update reverse function

--- src/reverse.js
+++ src/reverse.js
@@ -3,7 +3,11 @@
  * @param {string} text the string to be reversed
  */
 function reverse(text) {
- 
-}
+    if(text === "") {
+      return "";
+    } else {
+      return reverse(text.substring(1)) + text.charAt(0);
+    }
+   }
 
 module.exports = reverse;
